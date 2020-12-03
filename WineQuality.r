d757f5004be7bae250b3790a19d82541103a1aab
Stefanie <saloschiavo@gmail.com>
2020-12-02 16:35:00 -0800

    Update WineQuality.r

--- WineQuality.r
+++ WineQuality.r
@@ -20,7 +20,20 @@ summary(red)
 # using multiple linear regression to start
 lm.fit=lm(quality~., data=red)
 lm.fit
+# look at all variables in lm.fit object
+names(lm.fit)
+# summarize
+
+# TODO: GET OUT OLD NOTES AND EXAMINE THESE RESULTS
+
 summary(lm.fit)
+# note that small p-value indicates significance of multiple independent variables
+
+# plot the model itself
+plot(lm.fit)
+
+# TODO: WHAT IS GOING ON WITH THESE BONKERS GRAPHS
+# Note that the Residuals Vs. Leverage graph is......... interesting
 
 quality_red = red$quality
 
@@ -31,27 +44,102 @@ quality_red = red$quality
 # sulphates
 # alcohol
 
-plot(quality_red, red$`volatile acidity`, col='darkblue')
-plot(quality_red, red$chlorides)
-plot(quality_red, red$`total sulfur dioxide`)
-plot(quality_red, red$sulphates)
+plot(quality_red, red$`volatile acidity`, col='darkred')
+plot(quality_red, red$chlorides, col='darkred')
+plot(quality_red, red$`total sulfur dioxide`, col='darkred')
+plot(quality_red, red$sulphates, col='darkred')
 
 # A higher alcohol content appears to indicate a higher quality wine
-plot(quality_red, red$alcohol)
+plot(quality_red, red$alcohol, col='darkred')
 
-# TODO: GET OUT OLD NOTES AND EXAMINE THESE RESULTS
-
-# look at all variables in lm.fit object
-names(lm.fit)
+# NOte that R-squared is .3606, adjusted .3561
+# TODO: INTERPRET RESULT OF R-SQUARED
+# Recall that the R-squared statistic allows us to measure how our model performed versus had we not used a model at all.
+# The R-squared statistic of a linear model ... implies that/means 
 # to examine coefficients
 coef(lm.fit)
 # confidence intervals -- by default this is 95% CI
 ci=confint(lm.fit)
 ci
+
+# TODO: Rewatch videos on multiple linear regression models to see if there's more I can do
 # TODO: Interpret what these numbers say
+# Recall that F-statistic tests overall significance -- tests whether relationship is statistically significant
+
+# now let's find some outliers
+studentized_resi = studres(lm.fit)
+studentized_resi
+plot(studentized_resi)
+outlier_indices=which(abs(studentized_resi)>3)
+outlier_indices
+# so our problematic outlier rows are:
+# 46, 391, 441, 460, 653, 814, 833, 900, 1236, 1277, 1479, 1506
+
+# and high leverage points
+# this is calculated using cook's distance
+# calculate cd for every data point
+cd=cooks.distance(lm.fit)
+cd
+# extract points where cooks distance is > 4/n for high leverage points
+
+# how many observations in data?
+n=nrow(red)
+n
+# 1599 observations
+leverage_indices=which(cd>4/n)
+leverage_indices
+# there are quite a few here, so let us compare which overlap with our outliers
+# outlier *and* high leverage points are:
+# 46, 391, 441, 460, 653, 814, 833, 1236, 1277, 1479, 1506
+# all outliers are high leverage points but not all high leverage points are outliers
+# this is consistent with what we've learned about cooks distance
 
 
-# TODO: IDENTIFY AND EXAMINE OUTLIERS AND HIGH LEVERAGE POINTS
+# we can combine these indices and remove both quite easily
+remove_indices=union(outlier_indices,leverage_indices)
+remove_indices
+
+# check for high collinearity
+# this can be done using vif(), in the car package
+# install car package
+library(car)
+vif(lm.fit)
+
+# vif is larger than 5 for fixed acidity and density which is problematic
+# and may indicate collinearity issues
+# consider removing these predictors
+
+
+new_red=red[-remove_indices,]
+lm.fit3=lm(quality~.,data=new_red)
+summary(lm.fit3)
+
+# now we can reexamine significance
+# significant factors after removing outliers and high leverage points are:
+# volatile acidity
+# chlorides
+# total sulfur dioxide
+# sulphates
+# alcohol
+
+# these have remained the same
+plot(lm.fit3)
+
+# it looks more reasonable but NOTE THE FUNNEL SHAPE IN THE RESIDUALS VS LEVERAGE PLOT
+# This may indicate heteroskedasticity?
+# TODO: Let us remove the collinear predictors and try to improve the model.
+
+plot(lm.fit3$fitted.values,lm.fit3$residuals)
+
+# TODO: REMOVE THESE PREDICTORS AND RERUN?
+
+# what if we examined it as a logarithmic model?
+logfit=lm(log(quality)~., data=new_red)
+# still maintains most significant variables
+summary(logfit)
+plot(logfit$fitted.values,logfit$residuals)
+# this is a PROBLEM, there is no random distribution here whatsoever
+
 # TODO: Can we identify what makes a red wine quality 10?
 # TODO: Can we identify what makes a red wine quality 1?
 
@@ -95,9 +183,18 @@ coef(lm.fit2)
 ci2=confint(lm.fit2)
 ci2
 
-# TODO: IDENTIFY AND EXAMINE OUTLIERS AND HIGH LEVERAGE POINTS
+# IDENTIFY AND EXAMINE OUTLIERS AND HIGH LEVERAGE POINTS
+studentized_resi2 = studres(lm.fit2)
+studentized_resi2
+plot(studentized_resi2)
+
+outlier_indices2=which(abs(studentized_resi2)>3)
+outlier_indices2
+
+
+
+
 # TODO: Can we identify what makes a white wine quality 10?
 # TODO: Can we identify what makes a white wine quality 1?
 
 ```
-
